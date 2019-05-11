f002252ec922cc36cf2c5cf8732709c15823d7e6
Chuck Smith <chuck@polishedwp.com>
2019-05-11 17:10:02 -0400

    Styled Default page

--- src/pages/DefaultPage.js
+++ src/pages/DefaultPage.js
@@ -1,11 +1,19 @@
-import React, { Component } from "react";
+import React, { Component } from "react"
+import Hero from "../components/Hero"
+import defaultBcg from "../images/defaultBcg.jpeg"
+import { Link } from "react-router-dom"
 
 export default class DefaultPage extends Component {
   render() {
     return (
       <>
-        <h1>Hello from 404 Page</h1>;
+        <Hero img={defaultBcg} title="404">
+          <h2 className="text-uppercase">page not found</h2>
+          <Link to="/" className="main-link" style={{ margin: "2rem" }}>
+            return Home
+          </Link>
+        </Hero>
       </>
-    );
+    )
   }
 }
