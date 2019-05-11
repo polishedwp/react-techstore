e591f1fe6fcbe3286cc0011e1672cff26006de3b
Chuck Smith <chuck@polishedwp.com>
2019-05-10 20:22:08 -0400

    Router Setup

--- src/App.js
+++ src/App.js
@@ -1,31 +1,36 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
+import React, { Component } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-import About from "./pages/AboutPage";
-import Cart from "./pages/CartPage";
-import Contact from "./pages/ContactPage";
 import Home from "./pages/HomePage";
+import About from "./pages/AboutPage";
 import Products from "./pages/Products";
+import Contact from "./pages/ContactPage";
 import SingleProduct from "./pages/SingleProductPage";
+import Cart from "./pages/CartPage";
 import Error from "./pages/DefaultPage";
 
-function App() {
-  return (
-    <Router>
-      <main>
-        {/* <Navbar /> */}
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/recipes" exact component={Recipes} />
-          <Route path="/recipes/:id" component={SingleRecipe} />
-          <Route component={Error} />
-        </Switch>
-      </main>
-    </Router>
-  );
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <Router>
+          {/* navbar, sidebar, cart,footer*/}
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/contact" component={Contact} />
+            <Route path="/products" exact component={Products} />
+            <Route path="/products/:id" component={SingleProduct} />
+            <Route path="/cart" component={Cart} />
+            <Route component={Error} />
+          </Switch>
+        </Router>
+      </>
+    );
+  }
 }
 
 export default App;
